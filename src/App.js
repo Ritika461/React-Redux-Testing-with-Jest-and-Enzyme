d4cb09120a78e150b13e9d6984f6d45530c4ac59
Ritika461 <ritikapathak461@gmail.com>
2023-06-13 17:19:57 +0530

    Initial commit of components without tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import Header from './component/header'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Enzyme and jest</h1>
+        <Header />
       </div>
     );
   }
